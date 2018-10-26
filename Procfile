552f36ca2ca747cf925ccdd6437329b859b94b8e
BSINDIA <bsindia@bsindialab05.localdomain>
2018-10-26 01:50:03 -0400

    py file commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python initflask.py
---
+++ initflask.py
@@ -0,0 +1,22 @@
+
+import time
+import json
+from flask import Flask
+from werkzeug.wrappers import Request, Response
+from werkzeug.serving import run_simple
+
+app = Flask(__name__)
+
+@app.route("/f24/test",methods=['POST'])
+def f24Form():
+    return "Hellow"
+
+
+if __name__ == '__main__':
+        run_simple('localhost', 9000, app)
+
+	
+
+
+
+
