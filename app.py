92169433fc266ef42070b19999e64971cf978042
BSINDIA <bsindia@bsindialab05.localdomain>
2018-10-26 08:30:56 -0400

    skewimage update

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
 
 import time
 import json
-from flask import Flask
+import base64
+import cv2
+import numpy as np
+from flask import Flask,request
 from werkzeug.wrappers import Request, Response
 from werkzeug.serving import run_simple
+import imgskew
 
 app = Flask(__name__)
 
-@app.route("/f24/test",methods=['POST'])
+@app.route("/f24/api/imageskew",methods=['POST'])
 def f24Form():
-    return "Hellow"
+  data = request.json
+  print(data['encoded_img'])
+  result = imgskew.imageSkew(data['encoded_img']) 
+  #check(result)
+  response = json.dumps({'encodedImage':result.decode('UTF-8')})
+  return response
+
+def check(imgbase64):
+  nparr = np.fromstring(base64.b64decode(imgbase64), np.uint8)
+  skewed_image = cv2.imdecode(nparr, 1)
+  cv2.imwrite('./crossverify.jpg',skewed_image)
 
 
 if __name__ == '__main__':
-        #run_simple('localhost', 9000, app)
-        app.run(debug=True, use_reloader=True)
+  #run_simple('localhost', 9000, app)
+  app.run(debug=True, use_reloader=True)
 
 	
 
---
+++ imgskew.py
@@ -0,0 +1,63 @@
+import numpy as np
+import cv2
+import base64
+from matplotlib import pyplot as plt
+import math
+
+def imageSkew(imgbase64):
+  orig_image = cv2.imread('./F24_form.JPG', 0)
+  #skewed_image = cv2.imread('./img_skew.jpg', 0)
+  nparr = np.fromstring(base64.b64decode(imgbase64), np.uint8)
+  skewed_image = cv2.imdecode(nparr, 1)
+  #cv2.imwrite('./check.jpg',skewed_image)
+  #skewed_image = cv2.imread(base64.encode(imgbase64),0)
+  surf = cv2.xfeatures2d.SURF_create(400)
+  kp1, des1 = surf.detectAndCompute(orig_image, None)
+  kp2, des2 = surf.detectAndCompute(skewed_image, None)
+  
+  FLANN_INDEX_KDTREE = 0
+  index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+  search_params = dict(checks=50)
+  flann = cv2.FlannBasedMatcher(index_params, search_params)
+  matches = flann.knnMatch(des1, des2, k=2)
+  
+  # store all the good matches as per Lowe's ratio test.
+  good = []
+  for m, n in matches:
+    if m.distance < 0.7 * n.distance:
+      good.append(m)
+  
+  #print(good)
+  
+  MIN_MATCH_COUNT = 10
+  if len(good) > MIN_MATCH_COUNT:
+    src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+    
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+    
+    # see https://ch.mathworks.com/help/images/examples/find-image-rotation-and-scale-using-automated-feature-matching.html for details
+    ss = M[0, 1]
+    sc = M[0, 0]
+    scaleRecovered = math.sqrt(ss * ss + sc * sc)
+    thetaRecovered = math.atan2(ss, sc) * 180 / math.pi
+    print("Calculated scale difference: %.2f\nCalculated rotation difference: %.2f" % (scaleRecovered, thetaRecovered))
+    im_out = cv2.warpPerspective(skewed_image, np.linalg.inv(M), (orig_image.shape[1], orig_image.shape[0]))
+    #plt.imshow(im_out, 'gray')
+    #cv2.imwrite('./sc.jpg',im_out)
+    #plt.show()
+    # Convert captured image to JPG
+    retval, buffer = cv2.imencode('.jpg', im_out)
+
+    # Convert to base64 encoding and show start of data
+    jpg_as_text = base64.b64encode(buffer)
+    #print(jpg_as_text[:80])
+    #print(type(jpg_as_text))
+    return jpg_as_text
+    
+  else:
+    errorResult = "Not  enough  matches are found   -   %d/%d" % (len(good), MIN_MATCH_COUNT);
+    print(errorResult)
+    matchesMask = None
+    return errorResult
+
